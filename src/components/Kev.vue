df3aa40f1d5c6ef6cbb9f93a3719ffd526405c7f
Kevin <keviinnalvarez2015@gmail.com>
2020-09-09 15:36:44 -0400

    interpretación de datos

--- src/components/Kev.vue
+++ src/components/Kev.vue
@@ -1,11 +1,24 @@
 <template>
-  <h3>This is Component</h3>
+  <div>
+    <h2>Datos {{title}}</h2>
+    <h3>Your name is {{user.name}} {{user.last}}!</h3>
+    <div v-html='custom' />
+  </div>
 </template>
 
 <script>
-  export default {}
+export default {
+  // name: "",
+  data: () => ({
+    title: 'de Prueba',
+    user: {
+      name: 'Kevin',
+      last: 'Torres'
+    },
+    custom: '<h4>Custom</h4>'
+  })
+}
 </script>
 
 <style scoped>
-
 </style>

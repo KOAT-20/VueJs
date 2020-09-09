d08142d0636ab846be26b4ccd674588778cba2b7
Kevin <keviinnalvarez2015@gmail.com>
2020-09-09 15:20:07 -0400

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id='app'>
+    <!-- <img src="./assets/logo.png"> -->
+    <h1>Hello World!</h1>
+    <Kev />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Kev from './components/Kev.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Kev
   }
 }
 </script>
---
+++ src/components/Kev.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>This is Component</h3>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+
+</style>
